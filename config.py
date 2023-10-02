ed4d4d4822a1980e75c3d400d17840db7b680126
Phan Trung Hoa <bonth@Phans-MacBook-Pro.local>
2023-10-02 20:34:23 +0700

    Ingest Data

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+import json
+
+
+def get_config():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return config
--- ingestion.py
+++ ingestion.py
@@ -1,26 +1,65 @@
-import pandas as pd
-import numpy as np
 import os
-import json
+import glob
+import logging
+import numpy as np
+import pandas as pd
+from config import get_config
 from datetime import datetime
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
+LOGGER = logging.getLogger(__name__)
+CONFIG = get_config() 
 
+input_folder_path = CONFIG['input_folder_path']
+output_folder_path = CONFIG['output_folder_path']
+final_data_path = os.path.join(
+    os.getcwd(), 
+    output_folder_path, 
+    'final_data.csv'
+)
+ingested_files_path = os.path.join(
+    os.getcwd(), 
+    output_folder_path,
+    'ingestedfiles.txt'
+)
 
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-input_folder_path = config['input_folder_path']
-output_folder_path = config['output_folder_path']
-
-
-
-#############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
-
-
-
+def merge_multiple_dataframe(
+    input_folder_path,
+    ingested_files_path,
+    final_data_path
+):
+    data_path_list = glob.glob(
+        os.path.join(
+            os.getcwd(),
+            input_folder_path,
+            "*.csv"
+        )
+    )
+    data_name_list = [name.split("/")[-1] for name in data_path_list]
+    
+    LOGGER.info(f"Found input data files: {' '.join(data_name_list)})")
+    with open(ingested_files_path, "w") as f:
+        for name in data_name_list:
+            f.write(f"{name}\n")
+            
+    df = pd.concat(
+        [pd.read_csv(path) for path in data_path_list], 
+        ignore_index=True
+    )
+    
+    df_processed = df.drop_duplicates()
+    df_processed.to_csv(final_data_path, index=False)
+    return df_processed
+    
+    
+    
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    merge_multiple_dataframe(
+        input_folder_path=input_folder_path,
+        ingested_files_path=ingested_files_path,
+        final_data_path=final_data_path
+    )
